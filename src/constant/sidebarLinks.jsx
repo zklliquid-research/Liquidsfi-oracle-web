08fa9ff0366121a2ff04a2f6324db90a1bd3c9cf
Jideotetic <jideotetic@gmail.com>
2025-11-25 20:55:28 +0100

    Added supported chains page

--- src/constant/sidebarLinks.jsx
+++ src/constant/sidebarLinks.jsx
@@ -1,77 +1,35 @@
-import {
-  BitcoinRefresh,
-  Bubble,
-  Home2,
-  PercentageCircle,
-  Repeat,
-  EmptyWalletAdd,
-  Activity,
-  Game,
-  Image,
-  Link2,
-  Data,
-  BuyCrypto,
-  Award,
-} from "iconsax-react";
+import { Bubble, Repeat } from "iconsax-react";
 import { PiBinocularsBold, PiSwap } from "react-icons/pi";
 
 export default [
-  {
-    heading: "CROSS-CHAIN BRIDGE",
-    links: [
-      {
-        title: "Multichain Bridge",
-        icon: <Repeat />,
-        path: "https://bridge.liquids.fi/",
-      },
-      {
-        title: "Liquidity Protocol",
-        icon: <Bubble />,
-        path: "https://bridge.liquids.fi/liquidity",
-      },
-
-      // {
-      // 	title: "Collateralized Loans",
-      // 	icon: <Bubble />,
-      // 	path: "/loans",
-      // },
-    ],
-  },
-  {
-    heading: "ORACLE NETWORK",
-    links: [
-      {
-        title: "Explorer",
-        path: "https://explorer.liquids.fi/",
-        icon: <PiBinocularsBold className="text-[24px]" />,
-      },
-      {
-        title: "Lane Status",
-        path: "https://explorer.liquids.fi/lane-status",
-        icon: <PiSwap className="text-[24px]" />,
-      },
-    ],
-  },
-  // {
-  // 	title: "Explorer",
-  // 	icon: <Bubble />,
-  // 	path: "/explorer",
-  // },
-
-  // {
-  // 	title: "Lane Status",
-  // 	icon: <Bubble />,
-  // 	path: "/lane-status",
-  // },
-  // {
-  //   title: "Rewards and Quests",
-  //   icon: <Award />,
-  //   path: "/rewards",
-  // },
-
-  // {
-  //   title: "Faucet",
-  //   icon: <BuyCrypto />,
-  //   path: "/faucet",
-  // },
+	{
+		heading: "CROSS-CHAIN BRIDGE",
+		links: [
+			{
+				title: "Multichain Bridge",
+				icon: <Repeat />,
+				path: "https://bridge.liquids.fi/",
+			},
+			{
+				title: "Liquidity Protocol",
+				icon: <Bubble />,
+				path: "https://bridge.liquids.fi/liquidity",
+			},
+		],
+	},
+	{
+		heading: "ORACLE NETWORK",
+		links: [
+			{
+				title: "Explorer",
+				path: "https://explorer.liquids.fi/",
+				icon: <PiBinocularsBold className="text-[24px]" />,
+			},
+			{
+				title: "Supported chains",
+				path: "https://explorer.liquids.fi/supported-chains",
+				icon: <PiSwap className="text-[24px]" />,
+			},
+		],
+	},
 ];
